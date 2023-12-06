02a8086670ef94c5300f8ed5f013f691461470ef
Matt Creaser <mattwcc@amazon.com>
2023-12-06 10:59:32 -0400

    Make Day5 solution 30,000x faster

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,34 +1,66 @@
 private val input = readInputFile(5).lineSequence()
 private val regex = """(\d+) (\d+) (\d+)""".toRegex()
 private val seeds = "\\d+".toRegex().findAll(input.first()).map { it.value.toLong() }
-private val mappings = getMappings()
+private val stages = getStages()
 
 fun main() {
     runAttempt(::part1)
     runAttempt(::part2)
 }
 
-private fun part1() = seeds.toLocation(mappings).min()
+private fun part1() = seeds.toLocation(stages).min()
 private fun part2() = seeds
     .chunked(2)
-    .flatMap { it[0]..<it[0] + it[1] }
-    .toLocation(mappings)
-    .min()
+    .map { it[0]..<it[0] + it[1] }
+    .rangeToLocation(stages)
+    .minOf { it.first }
 
-private fun getMappings() = input.drop(2).fold(mutableListOf<MutableMap<LongRange, Long>>(mutableMapOf())) { mappings, line ->
+data class Stage(val mappings: MutableList<Mapping> = mutableListOf())
+data class Mapping(val range: LongRange, val increment: Long)
+
+private fun getStages() = input.drop(2).fold(mutableListOf(Stage())) { stages, line ->
     if (line.isEmpty()) {
-        mappings += mutableMapOf()
+        stages += Stage()
     } else {
         regex.matchEntire(line)?.let { match ->
             val (destination, source, size) = match.groupValues.drop(1).map { it.toLong() }
-            mappings.last()[source..<source + size] = destination
+            stages.last().mappings.add(Mapping(source..<source + size, destination - source))
         }
     }
-    mappings
-}.map { it.entries.map { entry -> entry.toPair() }.toTypedArray() }.toTypedArray()
+    stages.onEach { it.mappings.sortBy { mapping -> mapping.range.first } }
+}
 
-private fun Sequence<Long>.toLocation(mappings: Array<Array<Pair<LongRange, Long>>>) = map { input ->
-    mappings.fold(input) { value, mapping ->
-        mapping.firstOrNull { it.first.contains(value) }?.let { (range, inc) -> value - range.first + inc } ?: value
+private fun Sequence<Long>.toLocation(stages: List<Stage>) = map { input ->
+    stages.fold(input) { value, stage -> stage.mappings.firstOrNull { it.range.contains(value) }?.let { value + it.increment } ?: value }
+}
+
+private fun Sequence<LongRange>.rangeToLocation(stages: List<Stage>) = flatMap { range ->
+    stages.fold(sequenceOf(range)) { ranges, stage -> ranges.flatMap { it.mapInto(stage.mappings) } }
+}
+
+private fun LongRange.mapInto(mappings: List<Mapping>) = sequence {
+    var remaining = this@mapInto
+    for (mapping in mappings) {
+        val inc = mapping.increment
+        if (remaining.last < mapping.range.first) {
+            yield(remaining)
+            return@sequence
+        } else if (remaining.first > mapping.range.last) {
+            continue
+        }
+
+        if (remaining.first < mapping.range.first) {
+            yield(remaining.first..<mapping.range.first)
+        }
+
+        val range = maxOf(remaining.first, mapping.range.first) + inc..minOf(remaining.last, mapping.range.last) + inc
+        yield(range)
+
+        if (remaining.last > mapping.range.last) {
+            remaining = mapping.range.last + 1..remaining.last
+        } else {
+            return@sequence
+        }
     }
+    yield(remaining)
 }
