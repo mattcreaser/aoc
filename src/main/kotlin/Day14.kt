cdbbc7924add93ee13cee7ad5f78acea16d54466
Matt Creaser <mattwcc@amazon.com>
2023-12-14 21:13:35 -0400

    Make fewer grid allocations for day 14

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,5 +1,3 @@
-import java.util.*
-
 typealias RockGrid = Array<CharArray>
 
 class Day14(input: String? = null) : AdventOfCodeDay(input) {
@@ -7,14 +5,14 @@ class Day14(input: String? = null) : AdventOfCodeDay(input) {
     private val rows = lines.size
     private val cols = lines[0].length
 
-    override fun part1() = lines.toRockGrid().shiftedNorth().calculateWeight()
+    override fun part1() = lines.toRockGrid().shiftNorth().calculateWeight()
 
     override fun part2(): Int {
         val grids = mutableListOf(lines.toRockGrid())
         var cycle: IntRange? = null
 
         while (cycle == null) {
-            val newGrid = grids.last().shiftedNorth().shiftedWest().shiftedSouth().shiftedEast()
+            val newGrid = grids.last().deepCopy().shiftNorth().shiftWest().shiftSouth().shiftEast()
             for (i in grids.size - 1 downTo 0) {
                 if (newGrid contentDeepEquals grids[i]) cycle = i..<grids.size
             }
@@ -26,49 +24,49 @@ class Day14(input: String? = null) : AdventOfCodeDay(input) {
         return grids[cycle.first + remaining].calculateWeight()
     }
 
-    private fun RockGrid.shiftedNorth() = newGrid().also {
+    private fun RockGrid.shiftNorth() = apply {
         for (x in 0..<cols) {
             var next = 0
             for (y in 0..<rows) {
                 when (this[y][x]) {
-                    'O' -> it[next++][x] = 'O'
-                    '#' -> { it[y][x] = '#'; next = y + 1 }
+                    'O' -> { this[y][x] = '.'; this[next++][x] = 'O' }
+                    '#' -> next = y + 1
                 }
             }
         }
     }
 
-    private fun RockGrid.shiftedWest() = newGrid().also {
+    private fun RockGrid.shiftWest() = apply {
         for (y in 0..<rows) {
             var next = 0
             for (x in 0..<cols) {
                 when (this[y][x]) {
-                    'O' -> it[y][next++] = 'O'
-                    '#' -> { it[y][x] = '#'; next = x + 1 }
+                    'O' -> { this[y][x] = '.'; this[y][next++] = 'O' }
+                    '#' -> next = x + 1
                 }
             }
         }
     }
 
-    private fun RockGrid.shiftedSouth() = newGrid().also {
+    private fun RockGrid.shiftSouth() = apply {
         for (x in 0..<cols) {
             var next = rows - 1
             for (y in rows - 1 downTo 0) {
                 when (this[y][x]) {
-                    'O' -> it[next--][x] = 'O'
-                    '#' -> { it[y][x] = '#'; next = y - 1 }
+                    'O' -> { this[y][x] = '.'; this[next--][x] = 'O' }
+                    '#' -> next = y - 1
                 }
             }
         }
     }
 
-    private fun RockGrid.shiftedEast() = newGrid().also {
+    private fun RockGrid.shiftEast() = apply {
         for (y in 0..<rows) {
             var next = cols - 1
             for (x in cols - 1 downTo 0) {
                 when (this[y][x]) {
-                    'O' -> it[y][next--] = 'O'
-                    '#' -> { it[y][x] = '#'; next = x - 1 }
+                    'O' -> { this[y][x] = '.'; this[y][next--] = 'O' }
+                    '#' -> next = x - 1
                 }
             }
         }
@@ -76,7 +74,7 @@ class Day14(input: String? = null) : AdventOfCodeDay(input) {
 
     private fun RockGrid.calculateWeight() = this.foldIndexed(0) { i, sum, row -> sum + (this@calculateWeight.size - i) * row.count { it == 'O' } }
     private fun List<String>.toRockGrid() = map { it.toCharArray() }.toTypedArray()
-    private fun newGrid() = Array(rows) { CharArray(cols) { '.' } }
+    private fun RockGrid.deepCopy() = Array(size) { this[it].copyOf() }
 }
 
 fun main() = Day14().run()
